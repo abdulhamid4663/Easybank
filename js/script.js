0910fad3cc921a7ecdb79606ca3d118dc8b055cd
Abdulhamid H Johar <60272741+abdulhamid4663@users.noreply.github.com>
2021-07-13 00:26:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')) {
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    }
+    else {
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
